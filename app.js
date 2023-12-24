cd51e83c9dcefe940b34ff6d8003d69e1e12491c
Melody G <melodyflash@gmail.com>
2023-12-24 14:46:09 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ src/employees.jsx
@@ -0,0 +1,15 @@
+let sally = 'Sally Smith'
+let mark = 'Mark Martin'
+let holly = 'Holly Unlikely'
+let amol = 'Amol Shookup'
+let robin = 'Robin Banks'
+const element = (
+    <ul style={{'color':'blue', 'fontSize': '24px'}}>
+        <li>{sally}</li>
+        <li>{mark}</li>
+        <li>{holly}</li>
+        <li>{amol}</li>
+        <li>{robin}</li>
+    </ul>
+)
+ReactDOM.render(element, document.getElementById('content'))
